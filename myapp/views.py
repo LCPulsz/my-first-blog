be2836fe0a62d0db9e7b4008d8f90711e0aff45f
Lucas <lucas@vendabem.com.br>
2021-08-03 10:49:11 -0300

    final touch

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.utils import timezone
 from django.shortcuts import redirect
 
@@ -7,7 +7,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all() 
+    posts = Post.objects.order_by("-published_date").all() 
     return render(request, 'myapp/posts_list.html', {
         'posts': posts
     })
@@ -28,6 +28,27 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+
+        posts = Post.objects.all()
+        return render(request, 'myapp/posts_list.html', {'posts':posts})
     else:
-        form = PostForm()
-    return render(request, 'myapp/post_edit.html', {'form': form})
+        form = PostForm(Post)
+        return render(request, 'myapp/post_new.html', {'form': form})
+
+def post_edit(request, pk):
+    if request.method == "POST":
+        form = request.POST
+        post = Post.objects.get(pk=pk)
+
+        post.text = form['text']
+        post.published_date = timezone.now()
+        post.save()
+
+        posts = Post.objects.all()
+
+        return render(request, 'myapp/posts_list.html', {'posts':posts})
+
+    else:
+        post = Post.objects.get(pk=pk)
+        post = post
+        return render(request, 'myapp/post_edit.html', {'form': post})
